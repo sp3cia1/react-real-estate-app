c2b9097f19ef3c51694232ab4b8f0b232d9ac5b9
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2024-11-22 18:36:12 +0530

    created the card and filter components and completed the listings side of the list page using some dummy data

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,8 +1,22 @@
+import { listData } from '../../lib/dummydata.js';
 import './listPage.scss';
+import Filter from '../../components/filter/filter.jsx';
+import Card from '../../components/card/card.jsx';
 
 function ListPage(){
+    const data = listData;
     return (
-        <div className="listPage">List Page</div>
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter/>
+                    {data.map(item => (<Card key = {item.id} item = {item}/>))}
+                </div>
+            </div>
+            <div className="mapContainer">
+                Map
+            </div>
+        </div>
     )
 }
 
