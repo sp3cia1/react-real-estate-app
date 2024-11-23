4fc9d0d60e189be74d5a087b40b40d54a3049e32
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2024-11-23 15:14:14 +0530

    craeted the info section on our singlePage

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,18 @@
+import './slider.scss';
+
+function Slider({images}){
+    return (
+        <div className="slider">
+            <div className="bigImage">
+                <img src={images[0]} alt="" />
+            </div>
+            <div className="smallImages">
+                {images.slice(1).map((image, index) => (
+                    <img key={index} src={image} alt="" />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Slider;
